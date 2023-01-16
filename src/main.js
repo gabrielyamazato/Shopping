02789787f215872e6e8502dfe24fc631efff6c3a
Gabriel <gabrielyamazato@gmail.com>
2023-01-16 18:30:04 -0300

    requisito 3 ok

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+const produtoHTML = document.querySelector('.products');
+
+const productGrid = async () => {
+  const dataProductsList = await fetchProductsList('computer');
+
+  dataProductsList.forEach((element) => {
+    produtoHTML.appendChild(createProductElement(element));
+  });
+};
+productGrid();
