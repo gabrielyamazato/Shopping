2be974f0e498d1d4ca27080a5cb2f11bdb0aec37
Gabriel <gabrielyamazato@gmail.com>
2023-01-16 15:26:44 -0300

    requisito 2 ok

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (parametro) => {
+  if (parametro === '') {
+    throw new Error('Termo de busca não informado');
+  }
+
+  const resposta = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${parametro}`);
+  const dados = resposta.json();
+
+  return dados;
 };
