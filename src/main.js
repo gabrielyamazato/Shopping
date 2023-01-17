41a752ded024d6651ba07a670da362ff2c8cc3c6
Gabriel <gabrielyamazato@gmail.com>
2023-01-16 21:02:37 -0300

    requisito 4 ok

--- src/main.js
+++ src/main.js
@@ -6,11 +6,37 @@ import { createProductElement } from './helpers/shopFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const produtoHTML = document.querySelector('.products');
 
+const removeLoading = () => {
+  const getLoad = document.querySelector('.loading');
+  const getBody = document.querySelector('body');
+
+  getBody.removeChild(getLoad);
+};
+
 const productGrid = async () => {
   const dataProductsList = await fetchProductsList('computer');
 
   dataProductsList.forEach((element) => {
     produtoHTML.appendChild(createProductElement(element));
   });
+  removeLoading();
 };
-productGrid();
+
+const loading = () => {
+  const containerLoading = document.querySelector('body');
+  const x = document.createElement('h1');
+  x.className = 'loading';
+  x.innerText = 'carregando...';
+
+  containerLoading.appendChild(x);
+};
+
+function loadScreen() {
+  try {
+    loading();
+    productGrid();
+  } catch (error) {
+    console.log(error);
+  }
+}
+loadScreen();
