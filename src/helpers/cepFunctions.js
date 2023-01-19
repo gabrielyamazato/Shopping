842483bab7ba37a5956b1b2226080aa3cacc5277
Gabriel <gabrielyamazato@gmail.com>
2023-01-19 19:27:34 -0300

    11 halfway, levar pra mentoria

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,30 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (input) => {
+  try {
+    const API_1 = fetch(`https://cep.awesomeapi.com.br/json/${input}`);
+    const API_2 = fetch(`https://brasilapi.com.br/api/cep/v2/${input}`);
+
+    return Promise.any([API_1, API_2])
+      .then((primeiraResposta) => primeiraResposta.json())
+      .then((resposta) => {
+        if (resposta.neighborhood === true) {
+          return `${resposta.street} - ${resposta.neighborhood} - ${resposta.city} - ${resposta.state}`;
+        }
+        return `${resposta.address} - ${resposta.district} - ${resposta.city} - ${resposta.state}`;
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const searchCep = () => {
-  // seu código aqui
+const span = document.querySelector('.cart__adress');
+
+export const searchCep = async () => {
+  const input = document.querySelector('.cep-input').value;
+  const info = await getAddress(input);
+
+  if (info === 'undefined - undefined - undefined - undefined') {
+    return span.innerText = 'CEP não encontrado';
+  } else {
+    return span.innerText = info;
+  }
 };
